2da27d1d2085dc6275864626b1891128fc7cf01c
phattarapoll <phattarapoll@gmail.com>
2025-11-04 08:45:11 +0700

    update

---
+++ psvmt/SYS/config.php
@@ -0,0 +1,24 @@
+<?php
+
+// Bicubica PageProtect Script (Easily Protect Pages on your Site)
+// Written By Bicubica.com on April 30th, 2006
+// LICENSE: FREE WITH CREDIT GIVEN TO BICUBICA.COM
+
+######################################################
+#              	Configuration Settings		     #
+######################################################
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Login</title>
+
+//  Username AND Password 
+$adminuser = "user";
+$adminpass = "pass";
+
+// ใส่ URL ของหน้าที่ต้องการให้ไปหลังจาก Login สำเร็จ
+$redirectpage = "http://psvmt.6te.net/download.html";
+
+######################################################
+#           END OF CONFIGURATION SETTINGS	     #
+######################################################
+
+?>
---
+++ psvmt/config.php
@@ -0,0 +1,22 @@
+<?php
+
+// Bicubica PageProtect Script (Easily Protect Pages on your Site)
+// Written By Bicubica.com on April 30th, 2006
+// LICENSE: FREE WITH CREDIT GIVEN TO BICUBICA.COM
+
+######################################################
+#              	Configuration Settings		     #
+######################################################
+
+//  Username AND Password 
+$adminuser = "adm";
+$adminpass = "mda";
+
+// ใส่ URL ของหน้าที่ต้องการให้ไปหลังจาก Login สำเร็จ
+$redirectpage = "http://psvmt.6te.net/download.html";
+
+######################################################
+#           END OF CONFIGURATION SETTINGS	     #
+######################################################
+
+?>
