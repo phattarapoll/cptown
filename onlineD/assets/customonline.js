32646fc64de64eea7b647fced62561e5dab8d966
phattarapoll <phattarapoll@gmail.com>
2026-08-05 10:30:57 +0700

    Update customonline.js

--- onlineD/assets/customonline.js
+++ onlineD/assets/customonline.js
@@ -257,7 +257,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const today = new Date();
         today.setHours(0, 0, 0, 0);
         const bookingLimit = new Date();
-        bookingLimit.setDate(today.getDate() + 30);
+        bookingLimit.setDate(today.getDate() + 28);
         bookingLimit.setHours(0, 0, 0, 0);
 
         // ⚡ ใช้ DocumentFragment เพื่อความเร็วสูงสุดในการวาดปฏิทิน
